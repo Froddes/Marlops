75af1887407fd1907634f23d970dd710b043c95a
Froddes <froddes.dev@gmail.com>
2025-02-12 20:59:28 +0100

    viajes marlops card

--- src/js/main/main.js
+++ src/js/main/main.js
@@ -1,10 +1,26 @@
 "use strict";
 
-function main(){
+function main() {
     const viajes = document.getElementById("viajesmarlops");
-    document.getElementById("viajes-img").addEventListener("click", function () {
+    const viajesImg = document.getElementById("viajes-img");
+    const contents = document.querySelectorAll(".content");
+    const times = document.querySelector(".fa-times");
+
+    function toggleBlur() {
+        const blurValue = viajes.classList.contains("open") ? "3.5px" : "0";
+        contents.forEach(content => content.style.filter = `blur(${blurValue})`);
+    }
+
+    viajesImg.addEventListener("click", () => {
         viajes.classList.toggle("open");
+        toggleBlur();
+    });
+
+    times.addEventListener("click", () => {
+        viajes.classList.toggle("open");
+        toggleBlur();
     });
 }
+
 
 document.addEventListener("DOMContentLoaded", main);
