97b2a5d43f17db14df0550e6939baeee865ca05c
Froddes <froddes.dev@gmail.com>
2025-02-12 20:07:11 +0100

    prueba

--- src/js/main/main.js
+++ src/js/main/main.js
@@ -4,6 +4,7 @@ function main(){
     const viajes = document.getElementById("viajesmarlops");
     document.getElementById("viajes-img").addEventListener("click", function () {
         viajes.classList.toggle("open");
+        console.log("Open")
     });
 }
 
