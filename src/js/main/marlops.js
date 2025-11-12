97b7bf966e593601eec38e48524b8134d051ef82
Frarodmar19 <frarodmar19@alum.us.es>
2025-11-12 14:28:36 +0100

    trasquila gallery

--- src/js/main/marlops.js
+++ src/js/main/marlops.js
@@ -10,7 +10,6 @@ function marlops(){
     loadGallery();
     loadMenu();
     handleImages();
-    // handleVideos();
 }
 
 function loadGallery(){
@@ -46,42 +45,5 @@ function handleImages(){
         document.body.style.overflow = 'auto';
     }
 }
-
-// function handleImgs(){
-//     const imgs = document.querySelectorAll(".gallery-img");
-//     const viewImgContainer = document.getElementById("view-img-container");
-//     const viewImg = document.getElementById("view-img");
-//     imgs.forEach(img => img.onclick = () =>  {
-//         document.body.style.overflow = 'hidden';
-//         const src = img.querySelector("img").getAttribute("src");
-//         viewImgContainer.classList = "view";
-//         viewImg.setAttribute("src", src);
-//     })
-//     viewImgContainer.onclick = () => {
-//         viewImgContainer.classList = "";
-//         document.body.style.overflow = 'auto';
-//     }
-// }
-
-// function handleVideos(){
-//     const videos = document.querySelectorAll(".gallery-video");
-//     videos.forEach(video => {
-//         const span = video.querySelector("span");
-//         const vid = video.querySelector("video");
-//         vid.volume = 0;
-//         vid.playbackRate = 0.75;
-//         span.onclick = () => {
-//             if(span.classList == "icon solid fa-play"){
-//                 vid.loop = true;
-//                 vid.play();
-//                 span.classList = "icon solid fa-pause";
-//             }else if(span.classList == "icon solid fa-pause"){
-//                 vid.loop = false;
-//                 vid.pause();
-//                 span.classList = "icon solid fa-play";
-//             }
-//         }
-//     })
-// }
 
 document.addEventListener("DOMContentLoaded", marlops);
--- src/js/main/trasquila.js
+++ src/js/main/trasquila.js
@@ -1,13 +1,18 @@
 "use strict";
 
+import {galleryRenderer} from '../renders/gallery.js';
 import { menuRenderer } from '../renders/menu.js';
 import { experienceRenderer } from '../renders/experience.js';
 import menu from '../../json/trasquilaMenu.json';
 import exp from '../../json/experienceTrasquila.json';
+import imgs from '../../json/trasquilaGallery.json';
+
 
 function trasquila(){
     loadMenu();
     loadExperience();
+    loadGallery();
+    handleImages();
 }
 
 function loadMenu(){
@@ -25,6 +30,31 @@ function loadExperience(){
         container.appendChild(experienceRenderer.asGallery(exp));
     }catch(error){
         console.error("Error al cargar la seccion de experiencia completa", error);
+    }
+}
+
+function loadGallery(){
+    const container = document.getElementById("trasquila-gallery");
+    try{
+        container.appendChild(galleryRenderer.asGallery(imgs));
+    }catch(error){
+        console.error("Error al cargar la galeria", error);
+    }
+}
+
+function handleImages(){
+    const images = document.querySelectorAll(".masonry-item");
+    const viewImgContainer = document.getElementById("view-img-container");
+    const viewImg = document.getElementById("view-img");
+    images.forEach(img => img.onclick = () =>  {
+        document.body.style.overflow = 'hidden';
+        const src = img.querySelector("img").getAttribute("src");
+        viewImgContainer.classList = "view";
+        viewImg.setAttribute("src", src);
+    });
+    viewImgContainer.onclick = () => {
+        viewImgContainer.classList = "";
+        document.body.style.overflow = 'auto';
     }
 }
 
